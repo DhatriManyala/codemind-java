77f3556e750c7870d87c4a412921ba0d3cf71845
DhatriManyala <119917907+DhatriManyala@users.noreply.github.com>
2023-08-29 11:20:49 +0530

    import java.util.Scanner; public class disk{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int t = sc.nextInt();         int s = sc.nextInt();         int b = sc.nextInt();         long c =(t*s*b*512*2);         System.out.println(c);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class disk{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int s = sc.nextInt();
+        int b = sc.nextInt();
+        long c =(t*s*b*512*2);
+        System.out.println(c);
+        
+    }
+}
